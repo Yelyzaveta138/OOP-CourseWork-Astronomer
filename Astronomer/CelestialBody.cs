c5f48e60a9ee50221078b1d918581429e4c1fe98
Яцюк Єлизавета <yelyzaveta.iatsiuk@nure.ua>
2026-05-12 14:48:13 +0200

    Add CelestialBody class structure

---
+++ Astronomer/CelestialBody.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Astronomer
+{
+   
+    public class CelestialBody
+    {
+        
+        public string Name { get; set; }        
+        public string Type { get; set; }        
+        public double Distance { get; set; }    
+        public double Magnitude { get; set; }   
+
+        
+        public CelestialBody(string name, string type, double distance, double magnitude)
+        {
+            Name = name;
+            Type = type;
+            Distance = distance;
+            Magnitude = magnitude;
+        }
+    }
+}
