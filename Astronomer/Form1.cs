2e643309b9900fa5219533e0638cb0fedc13b1e1
Яцюк Єлизавета <yelyzaveta.iatsiuk@nure.ua>
2026-05-12 15:03:34 +0200

    Bind CelestialBody list to dgvAstronomy and set Ukrainian headers

--- Astronomer/Form1.cs
+++ Astronomer/Form1.cs
@@ -2,9 +2,34 @@ namespace Astronomer
 {
     public partial class Form1 : Form
     {
+
+        private System.ComponentModel.BindingList<CelestialBody> bodies = new System.ComponentModel.BindingList<CelestialBody>();
+
         public Form1()
         {
             InitializeComponent();
+
+
+            dgvAstronomy.DataSource = bodies;
+
+            
+            if (dgvAstronomy.Columns["Name"] != null)
+                dgvAstronomy.Columns["Name"].HeaderText = "Назва об'єкта";
+
+            if (dgvAstronomy.Columns["Type"] != null)
+                dgvAstronomy.Columns["Type"].HeaderText = "Тип тіла";
+
+            if (dgvAstronomy.Columns["Distance"] != null)
+                dgvAstronomy.Columns["Distance"].HeaderText = "Відстань (св. р.)";
+
+            if (dgvAstronomy.Columns["Magnitude"] != null)
+                dgvAstronomy.Columns["Magnitude"].HeaderText = "Яскравість (m)";
+
+            
+            dgvAstronomy.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            
+            bodies.Add(new CelestialBody("Полярна зоря", "Зірка", 433.0, 1.97));
         }
     }
 }
